c6a980c08c2c66cc7817eed77af717d49de84cbd
Francisco <kikopocampo@gmail.com>
2022-10-21 19:51:32 -0600

    init function

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-function sumItems(array) {
-  // Sum all the numbers in the array
-}
+const sumItems = array => {
+  let sum = 0;
+  for (let element of array) {
+    if (Array.isArray(element)) {
+      sum += sumItems(element);
+    } else {
+      sum += element;
+    }
+  }
+  return sum;
+};
 
 module.exports = sumItems;
